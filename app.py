776ea193bdeafd66a8362da6887622b354cf2630
Sabah Ummie <sabah.ummie@gmail.com>
2021-11-11 20:48:24 -0500

    new branch created. I have also edited the app.py file to make the program run on browser.

--- app.py
+++ app.py
@@ -25,3 +25,6 @@ def login():
     # the code below is executed if the request method
     # was GET or the credentials were invalid
     return render_template('login.html', error=error)
+
+if __name__ == "__main__":
+    app.run(debug=True)
