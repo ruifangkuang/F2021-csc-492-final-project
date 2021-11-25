fe4b3941c82a7d7886242ba69ae7789eecfe7fc8
Brennan-Richards <brennanr0210@gmail.com>
2021-11-25 18:56:11 -0500

    Server setup

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/brennanrichards/Desktop/School/F2021/CSC492/csc-492-project/venv/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ wsgi.py
@@ -0,0 +1,5 @@
+# NOTE: For more info on deploying to server, see this tutorial: https://dev.to/techparida/how-to-deploy-a-flask-app-on-heroku-heb
+
+from main import app
+if __name__ == "__main__":
+  app.run()
