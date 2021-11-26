96ef6ddeae02dd93e1aa95aa0fcd1c04672d82f2
Sabah Ummie <sabah.ummie@gmail.com>
2021-11-26 11:09:19 -0500

    new environment setup

--- venv/bin/django-admin.py
+++ venv/bin/django-admin.py
@@ -1,4 +1,4 @@
-#!/Users/brennanrichards/Desktop/School/F2021/CSC492/csc-492-project/venv/bin/python3.7
+#!/Users/sabah/Documents/Projects/csc-492-project/venv/bin/python3
 from django.core import management
 
 if __name__ == "__main__":
