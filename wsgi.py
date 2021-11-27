98030747b111cc937e4e2b2f7429997691182e8d
Sabah Ummie <sabah.ummie@gmail.com>
2021-11-26 20:29:23 -0500

    debug set to true

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,6 @@
 # NOTE: For more info on deploying to server, see this tutorial: https://dev.to/techparida/how-to-deploy-a-flask-app-on-heroku-heb
 
 from main import app
+
 if __name__ == "__main__":
-  app.run()
+  app.run(debug=True)
